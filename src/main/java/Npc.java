bf064f7b2bfbc25593b27599babff8a0d8a61f0a
Thiago Cardoso <thiagoc1415@gmail.com>
2023-10-06 12:23:05 -0300

    feat: adicionando conteúdo nas classes

--- src/main/java/Npc.java
+++ src/main/java/Npc.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class Npc {
+public class Npc implements Cloneable  {
+
+    private String name;
+    private int age;
+    private String lore;
+
+    public Npc(String name, int age, String lore){
+        this.name = name;
+        this.age = age;
+        this.lore = lore;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getLore() {
+        return lore;
+    }
+
+    public void setLore(String lore) {
+        this.lore = lore;
+    }
+    @Override
+    public Npc clone() throws CloneNotSupportedException {
+        try {
+            return (Npc) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+
+    @Override
+    public String toString(){
+        return "NPC:" +
+                " Nome:" + name +
+                " Age:" + age +
+                " Lore:" + lore;
+    }
 }
--- src/test/java/NpcTest.java
+++ src/test/java/NpcTest.java
@@ -1,4 +1,19 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class NpcTest {
-  
+
+    @Test
+    void testClone() throws CloneNotSupportedException {
+        Npc npc1 = new Npc("Argit", 22, "He is a brave warrior");
+
+        Npc npc2 = npc1.clone();
+        npc2.setName("Kevin");
+        npc2.setAge(35);
+        npc2.setLore("He is a wise wizard");
+
+        assertEquals("NPC: Nome:Argit Age:22 Lore:He is a brave warrior", npc1.toString());
+        assertEquals("NPC: Nome:Kevin Age:35 Lore:He is a wise wizard", npc2.toString());
+    }
 }
